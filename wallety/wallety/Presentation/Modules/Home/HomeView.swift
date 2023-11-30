b97cd9de6bccd50b2fe15d99f5fdf09dbac501ec
Fernando Salom <fer@rudo.es>
2023-11-30 19:12:20 +0100

    Add home screen

---
+++ wallety/wallety/Presentation/Modules/Home/HomeView.swift
@@ -0,0 +1,93 @@
+//
+//  HomeView.swift
+//  wallety
+//
+//  Created by Fernando Salom Carratala on 30/11/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        HomeProfileView().padding(.horizontal, 20)
+        ScrollView(.vertical, showsIndicators: false) {
+            CurrentAmountView().padding(.horizontal, 20)
+            HorizontalListFavoriteAssetsView()
+            Text("Assets").padding(.horizontal, 20)
+            ListAssetsView().padding(.horizontal, 20)
+        }
+    }
+}
+
+struct HomeProfileView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "house").padding(10).background(Color.gray).clipShape(Circle())
+            Spacer()
+        }
+    }
+}
+
+struct CurrentAmountView: View {
+    var body: some View {
+        VStack(alignment: .leading, content: {
+            Text("Valor de la cartera:")
+            HStack {
+                Text("39.000€")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Image(systemName: "arrow.up.forward")
+                Spacer()
+            }
+        })
+    }
+}
+
+struct HorizontalListFavoriteAssetsView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 10, content: {
+                ForEach(1...10, id: \.self) { count in
+                    VStack {
+                        Text("hola")
+                    }
+                    .frame(width: 150, height: 200)
+                    .background(Color.gray)
+                    .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 10)))
+                }
+            })
+        }.contentMargins(.horizontal, 20, for: .scrollContent)
+    }
+}
+
+struct ListAssetsView: View {
+    var body: some View {
+        LazyVStack(spacing: 20, content: {
+            ForEach(1...10, id: \.self) { count in
+                HStack {
+                    Image(systemName: "house")
+                        .padding(10)
+                        .background(Color.white)
+                        .clipShape(Circle())
+                    VStack(alignment: .leading, content: {
+                        Text("Bitcoin")
+                        Text("BTC")
+                    })
+                    Spacer()
+                    VStack(alignment: .trailing, content: {
+                        Text("36.541,00€")
+                        Text("+12,23%")
+                    })
+                }
+                .padding(10)
+                .background(Color.gray)
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
+
+            }
+        })
+    }
+}
+
+#Preview {
+    HomeView()
+}
