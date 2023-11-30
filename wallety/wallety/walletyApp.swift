ca2ccd421db302343124a28276218ec7e23fe9c8
Fernando Salom <fer@rudo.es>
2023-11-30 14:05:03 +0100

    Add initial project

---
+++ wallety/wallety/walletyApp.swift
@@ -0,0 +1,32 @@
+//
+//  walletyApp.swift
+//  wallety
+//
+//  Created by Fernando Salom Carratala on 30/11/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct walletyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
