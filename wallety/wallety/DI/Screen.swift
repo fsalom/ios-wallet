6d2f1e174b450ff91949c51b3c24b10f65d134da
Fernando Salom <fer@rudo.es>
2023-12-29 13:31:46 +0100

    Add screen entity

---
+++ wallety/wallety/DI/Screen.swift
@@ -0,0 +1,14 @@
+//
+//  Screen.swift
+//  wallety
+//
+//  Created by Fernando Salom Carratala on 28/12/23.
+//
+
+import Foundation
+
+enum Screen {
+    case onBoarding
+    case app
+    case none
+}
