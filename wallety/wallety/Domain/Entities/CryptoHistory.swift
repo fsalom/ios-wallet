9f98d05cc75140ee60d682d522a2edba66fd35cb
Fernando Salom <fer@rudo.es>
2024-01-10 15:07:50 +0100

    Add new properties day and dayAndHour

--- wallety/wallety/Domain/Entities/CryptoHistory.swift
+++ wallety/wallety/Domain/Entities/CryptoHistory.swift
@@ -11,18 +11,28 @@ class CryptoHistory {
     var time: Int
     var priceUsd: Float
     var crypto: String = ""
-    var dateString: String {
-        let date = Date(timeIntervalSince1970: TimeInterval(time/1000))
-        let dateFormatter = DateFormatter()
-        dateFormatter.timeStyle = DateFormatter.Style.medium //Set time style
-        dateFormatter.dateStyle = DateFormatter.Style.medium //Set date style
-        dateFormatter.timeZone = .current
-        dateFormatter.dateFormat = "YYYY-MM-dd"
-        return dateFormatter.string(from: date)
+    var day: String {
+        time.toString(with: "YYYY-MM-dd")
+    }
+
+    var dayAndHour: String {
+        time.toString(with:  "YYYY-MM-dd HH:mm")
     }
 
     init(time: Int, priceUsd: Float) {
         self.time = time
         self.priceUsd = priceUsd
+    }
+}
+
+fileprivate extension Int {
+    func toString(with format: String) -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(self/1000))
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = DateFormatter.Style.medium
+        dateFormatter.dateStyle = DateFormatter.Style.medium
+        dateFormatter.timeZone = .current
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: date)
     }
 }
