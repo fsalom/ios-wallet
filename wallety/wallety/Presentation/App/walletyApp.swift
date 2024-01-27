d16bde6eba80caf536302459981bf00b2eefc7c6
Fernando Salom <fer@rudo.es>
2024-01-28 00:13:28 +0100

    Add firebase configuration

--- wallety/wallety/Presentation/App/walletyApp.swift
+++ wallety/wallety/Presentation/App/walletyApp.swift
@@ -9,25 +9,49 @@ import SwiftUI
 import SwiftData
 
 @main
-struct walletyApp: App {
+struct WalletyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var container: ModelContainer! = {
-            do {
-                let container = try ModelContainer(for: CryptoDBO.self, CryptoPortfolioDBO.self, RateDBO.self, FavoriteCryptoDBO.self)
-                if let url = container.configurations.first?.url.path(percentEncoded: false) {
-                    print("🗄️ sqlite3 \"\(url)\"")
-                } else {
-                    print("🗄️ No SQLite database found.")
-                }
-                return container
-            } catch {
-                fatalError("ERROR")
+        do {
+            let container = try ModelContainer(for: CryptoDBO.self,
+                                               CryptoPortfolioDBO.self,
+                                               RateDBO.self,
+                                               FavoriteCryptoDBO.self)
+            if let url = container.configurations.first?.url.path(percentEncoded: false) {
+                print("🗄️ sqlite3 \"\(url)\"")
+            } else {
+                print("🗄️ No SQLite database found.")
             }
-        }()
+            return container
+        } catch {
+            fatalError("ERROR")
+        }
+    }()
 
     var body: some Scene {
         WindowGroup {
             SplashBuilder().build()
+                .onAppear(perform: UIApplication.shared.addTapGestureRecognizer)
         }
-        .modelContainer(container)        
+        .modelContainer(container)
     }
 }
+
+extension UIApplication {
+    func addTapGestureRecognizer() {
+        guard let window = windows.first else { return }
+        let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+        tapGesture.requiresExclusiveTouchType = false
+        tapGesture.cancelsTouchesInView = false
+        tapGesture.delegate = self
+        window.addGestureRecognizer(tapGesture)
+    }
+}
+
+extension UIApplication: UIGestureRecognizerDelegate {
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return false // set to `false` if you don't want to detect tap during other gestures
+    }
+}
+
