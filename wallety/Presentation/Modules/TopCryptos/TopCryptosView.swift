9d768803ff1832254f446dd4d64709b0e13d8909
Fernando Salom <fer@rudo.es>
2024-03-20 12:24:46 +0100

    Add change percentage design

--- wallety/Presentation/Modules/TopCryptos/TopCryptosView.swift
+++ wallety/Presentation/Modules/TopCryptos/TopCryptosView.swift
@@ -33,39 +33,51 @@ struct TopCryptosView: View {
 
     @ViewBuilder
     func CryptoRow(with crypto: Crypto) -> some View {
-        HStack {
-            ZStack {
-                KFAnimatedImage(crypto.imageUrl)
-                    .frame(width: 50, height: 50)
-                    .background(Color.white)
-                    .clipShape(Circle())
-                if crypto.isFavorite {
-                    Image(systemName: "star.fill")
-                        .resizable()
-                        .frame(width: 15, height: 15)
-                        .foregroundColor(.yellow)
-                        .padding(5)
-                        .background(.white)
+        VStack(spacing: 0) {
+            HStack(spacing: 10) {
+                ZStack {
+                    KFAnimatedImage(crypto.imageUrl)
+                        .frame(width: 40, height: 40)
+                        .background(Color.white)
                         .clipShape(Circle())
-                        .offset(x: 15, y: 15)
-                        .shadow(radius: 1)
+                    if crypto.isFavorite {
+                        Image(systemName: "star.fill")
+                            .resizable()
+                            .frame(width: 12, height: 12)
+                            .foregroundColor(.yellow)
+                            .padding(5)
+                            .background(.white)
+                            .clipShape(Circle())
+                            .offset(x: 12, y: 12)
+                            .shadow(radius: 1)
 
+                    }
                 }
+                VStack(alignment: .leading, content: {
+                    Text(crypto.name)
+                        .fontWeight(.bold)
+                    Text(crypto.symbol)
+                        .font(.footnote)
+                })
+                Spacer()
+                VStack(alignment: .trailing, spacing: 0, content: {
+                    Text("\(crypto.price)")
+                        .fontWeight(.bold)
+                    Text("\(crypto.changePercent24HrFormatted)%")
+                        .foregroundStyle(crypto.changePercent24Hr > 0 
+                                         ? .green : .red)
+                        .fontWeight(.bold)
+                        .font(.footnote)
+                        .padding(4)
+                        .background(crypto.changePercent24Hr > 0 ?
+                                    Color.flagGreenBackground :
+                                    Color.flagRedBackground)
+                        .clipShape(RoundedRectangle(cornerRadius: 4))
+                })
             }
-            VStack(alignment: .leading, content: {
-                Text(crypto.name)
-                    .fontWeight(.bold)
-                Text(crypto.symbol)
-            })
-            Spacer()
-            VStack(alignment: .trailing, content: {
-                Text("\(crypto.price)")
-                    .fontWeight(.bold)
-                Text("\(crypto.changePercent24HrFormatted)%")
-            })
+            .padding(10)
+            Divider()
         }
-        .padding(10)
-        .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
     }
 }
 
